2c5720ba78dc144f817994fb4705dcd1781aa3c1
manuelblandino807 <m.b.blando@hotmail.com>
2026-07-11 16:01:45 +0200

    Aggiorna homepage BM Business

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // B-M Business Scripts
+const params = new URLSearchParams(window.location.search);
+
+const nome = params.get('nome') || '';
+const descrizione = params.get('descrizione') || '';
+const telefono = params.get('telefono') || '';
+const email = params.get('email') || '';
+const sito = params.get('sito') || '';
+
+console.log({
+  nome,
+  descrizione,
+  telefono,
+  email,
+  sito,
+});
